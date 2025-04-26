e7e4edd93d3777b5c923507295fb6a74083342e5
pngan <4557674+pngan@users.noreply.github.com>
2025-04-26 09:14:34 +0000

    Added restart for centraldb-migration

--- setup-transactionnotes.sh
+++ setup-transactionnotes.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 docker compose down apiservice
 docker compose down webfrontend
+docker compose down centraldb-migration
 docker compose pull apiservice
 docker compose pull webfrontend
+docker compose pull centraldb-migration
 
 docker compose up -d
